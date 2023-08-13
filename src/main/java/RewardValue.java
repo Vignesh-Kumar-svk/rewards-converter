65297241fd5b7b7f74f40312c0a52962da63bb87
Vignesh kumar.S <vigneshkumar.s.2020.cse@ritchennai.edu.in>
2023-08-13 22:06:01 +0530

    'third'

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -8,16 +8,33 @@ public class RewardValue {
 
 	}
 	public RewardValue(int milesValue){
-		this.miles=milesValue;
+		this.cash=convertToCash(milesValue);
 
 	}
 	
+	private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / 0.0035);
+    }
+	
+	private static double convertToCash(int milesValue) {
+	        return milesValue * 0.0035;
+	    }
 	public double getCashValue() {
 		return cash;
 	}
 	public int getMilesValue() {
-		return (int) (cash/0.0035);
+		return convertToMiles(cash);
 	}
+	
+	
+	
+	    
+
+	  
+
+	  
+
+	    
 	
 }
 	
